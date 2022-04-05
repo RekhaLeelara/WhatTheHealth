189b8a1ad56077107d84b974ab0c6a742ed2cd6b
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-04-04 22:08:00 -0400

    waiting room

--- models/test_result.js
+++ models/test_result.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize,DataTypes){
             allowNull: false,
         }
     },
-    {
+    
     patientID: {
         type: DataTypes.INTEGER,
         allowNull: false
@@ -33,4 +33,4 @@ TestResult.associate = (models)=> {
     })
 }
 
-return TestResult; 
+return TestResult;
