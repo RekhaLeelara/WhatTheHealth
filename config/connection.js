ecf694582d6b765f72c231a9520529dd2df8625a
Michael Palumbo <michaelpalumbo@users.noreply.github.com>
2022-04-10 17:32:12 -0400

    attempt heroku sequelize update

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,24 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-
-const sequelize = new Sequelize(
-  ('heroku_9e5f34d61cc7968'||process.env.DB_NAME),
-  ('b96b8e82fbeac3'||process.env.DB_USER),
-  ('30ee3ab5'||process.env.DB_PASSWORD),
-  {
-    host: ('us-cdbr-east-05.cleardb.net'||'localhost'),
-    dialect: 'mysql',
-    port: (process.env.PORT||process.env.DB_PORT),
-  }
-);
+let sequelize;
+if (process.env.DATABASE_URL){
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  }) 
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: process.env.DB_PORT,
+      }
+    );
+   
+}
 
 module.exports = sequelize;
-
-// * b96b8e82fbeac3: username
-// * 30ee3ab5: password
-// * us-cdbr-east-05.cleardb.net: host
-// * heroku_9e5f34d61cc7968: database
