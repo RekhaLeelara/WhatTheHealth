a9cb21dbbfacdb0706d0025eaa74c9bab1023174
Michael Palumbo <michaelpalumbo@users.noreply.github.com>
2022-04-10 12:52:45 -0400

    fix multiple headers problem by removing duplicate route instances

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,36 +6,8 @@ router.get('/', async (req, res) => {
   res.render('landing');
 });
 
-router.get('/user', (req, res) => {
-res.render('signup')
-});
-
-router.get('/', (req, res) => {
-  res.render('home')
-});
-
 router.get('/about', (req, res) => {
   res.render('about')
 });
   
-
-// POST /api/users
-// router.post('/user', (req, res) => {
-//   console.log("posted successfully");
-//   User.create({
-//     username: req.body.username,
-//     password: req.body.password
-//   })
-//   .then(dbUserData => {
-//     req.session.save(() => {
-//       req.session.username = dbUserData.username;
-//       req.session.user_id = dbUserData.id;
-//       req.session.loggedIn = true;
-  
-//       res.json(dbUserData);
-//     });
-
-//   });
-// });
-
 module.exports = router;
