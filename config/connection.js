e4e232f8ece5bbe356185e250fff8e48bf03842c
RekhaLeelara <62070645+RekhaLeelara@users.noreply.github.com>
2022-04-10 12:53:11 -0500

    updating the code to handle heroku db connection

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,19 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  (process.env.DB_NAME||'heroku_9e5f34d61cc7968'),
+  (process.env.DB_USER||'b96b8e82fbeac3'),
+  (process.env.DB_PASSWORD||'30ee3ab5'),
   {
-    host: 'localhost',
+    host: ('localhost'||'us-cdbr-east-05.cleardb.net'),
     dialect: 'mysql',
-    port: process.env.DB_PORT,
+    port: (process.env.DB_PORT||'process.env.PORT'),
   }
 );
 
 module.exports = sequelize;
+
+// * b96b8e82fbeac3: username
+// * 30ee3ab5: password
+// * us-cdbr-east-05.cleardb.net: host
+// * heroku_9e5f34d61cc7968: database
