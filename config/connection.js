6d5cd74745e83ea7f7fec32a86af81dd64eb7d34
RekhaLeelara <62070645+RekhaLeelara@users.noreply.github.com>
2022-04-10 12:59:19 -0500

    updated db  connection

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  (process.env.DB_NAME||'heroku_9e5f34d61cc7968'),
-  (process.env.DB_USER||'b96b8e82fbeac3'),
-  (process.env.DB_PASSWORD||'30ee3ab5'),
+  ('heroku_9e5f34d61cc7968'||process.env.DB_NAME),
+  ('b96b8e82fbeac3'||process.env.DB_USER),
+  ('30ee3ab5'||process.env.DB_PASSWORD),
   {
-    host: ('localhost'||'us-cdbr-east-05.cleardb.net'),
+    host: ('us-cdbr-east-05.cleardb.net'||'localhost'),
     dialect: 'mysql',
-    port: (process.env.DB_PORT||'process.env.PORT'),
+    port: (process.env.PORT||process.env.DB_PORT),
   }
 );
 
